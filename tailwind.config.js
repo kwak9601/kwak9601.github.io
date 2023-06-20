8a259df035af6722f1555e635e7013f6f33042dc
kwak9601 <kwak9601@gmail.com>
2023-06-20 07:35:30 -0600

    update: wordings for experience

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/css/*.{html,js}", "./dist/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
